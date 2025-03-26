e3dcca6c1197fcdffaabf7b6fd61f0c0372e1509
Ivo Dolenc <ivodolenc@gmail.com>
2025-03-26 15:04:34 +0100

    chore: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,4 @@
+import { jsConfig, tsConfig, svelteConfig, ignoresConfig } from './src/index.js'
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [jsConfig, tsConfig, svelteConfig, ignoresConfig]
