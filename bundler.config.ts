dc1bf2df329f4fba28909c6408ab77cbbd368984
Ivo Dolenc <ivodolenc@gmail.com>
2026-01-16 11:38:11 +0100

    chore: update externals

--- bundler.config.ts
+++ bundler.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from '@hypernym/bundler'
 import { dependencies, devDependencies } from './package.json'
 
-const externals = [
-  ...Object.keys(dependencies),
-  ...Object.keys(devDependencies),
-  'eslint/config',
-]
-
 export default defineConfig({
-  externals,
+  externals: [
+    ...Object.keys(dependencies),
+    ...Object.keys(devDependencies),
+    'eslint/config',
+  ],
   entries: [
     { input: './src/index.js' },
     { dts: './src/types.ts', output: './dist/index.d.ts' },
